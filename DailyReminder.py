ee942299f32f240b2cb15d94da1577a231389483
init-AK <akshath1015@gmail.com>
2024-03-21 18:55:55 -0500

    First Push

---
+++ DailyReminder.py
@@ -0,0 +1,12 @@
+from plyer import notification
+import time
+
+while True:
+    print("Attempting to show notification...")  # Debugging message
+    notification.notify(
+        title='REMINDER- 100DaysOfCode',
+        message='Did you do your Code Sessions Today?',
+        app_name='DAILY WORK REMINDER',
+        timeout=20
+    )
+    time.sleep(28800)
